82a314063a695f37dee673d7e1590f35499d6260
MSFCYP <88134548+MSFCYP@users.noreply.github.com>
2021-11-13 13:36:33 +0800

    第三周作业

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function () {
-    $("#bannerInner").tyslide({
+    $("#banner").tyslide({
         boxh:420,//盒子的高度
         w:1000,//盒子的宽度
         h:390,//图片的高度
@@ -13,5 +13,17 @@ $(function () {
         controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
         isShowNum:true //是否显示数字
     });
-    
+    // 图书电子书轮播
+    $("#ebooks-banner").tyslide({
+        boxh:223,//盒子的高度
+        w:332,//盒子的宽度
+        h:223,//图片的高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:20,//控制按钮宽度
+        controlsH:2,//控制按钮高度
+        controlsColor:"#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor:"green",//当前控制按钮的颜色
+    });
 })
